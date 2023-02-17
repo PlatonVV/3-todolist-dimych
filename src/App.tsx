d0eacfe397ba29927529b96b0b8c7785eeee7b9a
PlatonVV <platonvolodin@icloud.com>
2023-02-17 13:29:00 -0500

    addTasks done and input, state, done

--- src/App.tsx
+++ src/App.tsx
@@ -18,8 +18,8 @@ function App() {
     let filteredTasks = tasks.filter((t) => t.id !== id);
     setTasks(filteredTasks);
   }
-  function addTask() {
-    let newTask = { id: v1(), title: "New Task", isDone: false };
+  function addTask(title: string) {
+    let newTask = { id: v1(), title: title, isDone: false };
     setTasks([newTask, ...tasks]);
   }
 
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 import { FilterValuesType } from "./App";
 
 type TaskType = {
@@ -12,16 +12,21 @@ type PropsType = {
   tasks: Array<TaskType>;
   removeTask: (taskId: string) => void;
   changeFilter: (value: FilterValuesType) => void;
-  addTask: () => void;
+  addTask: (title: string) => void;
 };
 
 export function Todolist(props: PropsType) {
+  const [newTaskTitle, setNewTaskTitle] = useState("");
+
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setNewTaskTitle(e.currentTarget.value);
+  };
   return (
     <div>
       <h3>{props.title}</h3>
       <div>
-        <input />
-        <button onClick={() => props.addTask()}>+</button>
+        <input value={newTaskTitle} onChange={onChangeHandler} />
+        <button onClick={() => props.addTask(newTaskTitle)}>+</button>
       </div>
       <ul>
         {props.tasks.map((t) => (
